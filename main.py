20260ec89564a6d330e77569a495bd36fc751bcf
Nikhil Jha <say2nikhiljha@gmail.com>
2022-09-04 00:48:17 +0530

    change in test

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ var1 = sys.argv[1]
 a = 50
 def main():
     if var1:
-        print('Hello World!')
-    else:
-        print('No Way Here, raising a conflict')
+        print('Hello World, is it fine now?')
 
 main()
