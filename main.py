e1dc35cf94b07fa8fca87376f610cf74f238acec
Nikhil Jha <say2nikhiljha@gmail.com>
2022-09-04 00:45:55 +0530

    changes in main

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import sys
 
 var1 = sys.argv[1]
 
+a = 150
+b = 20
+a,b = b,a
+
 def main():
     if var1:
         print('Hello World!')
-    else:
-        print('No Way Here')
+    #else:
+    #    print('No Way Here')
 
 main()
