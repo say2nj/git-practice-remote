853b91a4e66aea890f39a3a86b0187f10d253f15
Nikhil Jha <say2nikhiljha@gmail.com>
2022-09-04 14:17:56 +0530

    stash changes

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ var1 = sys.argv[2]
 
 def main():
     for i in range(var0):
-<<<<<<< Updated upstream
-        print("Hi")
-=======
         print(f"Number: {i}")
->>>>>>> Stashed changes
 
 main()
